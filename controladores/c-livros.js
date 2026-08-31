e1eca55b2bb1e2892fd32a3b7c05bbae1ac0c217
EllieKonuma <elliekonuma@gmail.com>
2026-08-30 22:18:36 -0300

    feat: implement patch functionality for livro and update routes

--- controladores/c-livros.js
+++ controladores/c-livros.js
@@ -2,14 +2,17 @@ const {
   getTodosLivros,
   getLivroPorId,
   insereLivro,
+  modificaLivro,
 } = require("../servicos/s-livros");
 
 function getLivros(req, res) {
   try {
     const livros = getTodosLivros();
+
     res.send(livros);
   } catch (error) {
     res.status(500);
+
     res.send(error);
   }
 }
@@ -17,10 +20,13 @@ function getLivros(req, res) {
 function getLivro(req, res) {
   try {
     const id = req.params.id;
+
     const livro = getLivroPorId(id);
+
     res.send(livro);
   } catch (error) {
     res.status(500);
+
     res.send(error.message);
   }
 }
@@ -28,11 +34,34 @@ function getLivro(req, res) {
 function postLivro(req, res) {
   try {
     const livroInserido = insereLivro(req.body);
+
     res.status(201);
+
     res.send(livroInserido);
   } catch (error) {
     res.status(500);
+
     res.send({ error });
+  }
+}
+
+function patchLivro(req, res) {
+  try {
+    const id = req.params.id;
+
+    const body = req.body;
+
+    const modificado = modificaLivro(body, id);
+
+    if (!modificado) {
+      return res.status(404).send({ error: "Livro não encontrado" });
+    }
+
+    res.send(modificado);
+  } catch {
+    res.status(500);
+    
+    res.send({ error: "Erro ao modificar livro" });
   }
 }
 
@@ -40,4 +69,5 @@ module.exports = {
   getLivros,
   getLivro,
   postLivro,
+  patchLivro,
 };
--- servicos/s-livros.js
+++ servicos/s-livros.js
@@ -15,25 +15,34 @@ function getTodosLivros() {
 
 function getLivroPorId(id) {
   const livros = readJson();
+
   const livroFiltrado = livros.find((livro) => String(livro.id) === String(id));
   return livroFiltrado;
 }
 
 function insereLivro(livroNovo) {
   const livros = readJson();
+
   const novaListaDeLivros = [...livros, livroNovo];
+
   writeJson(novaListaDeLivros);
   return livroNovo;
 }
 
 function modificaLivro(modificacoes, id) {
-  // const livrosAtuais = readJson();
-  // const indiceModificado = livrosAtuais.findIndex((livro) => String(livro.id) === String(id));
-  // if (indiceModificado === -1) return null;
-  // const conteudoMudado = { ...livrosAtuais[indiceModificado], ...modificacoes };
-  // livrosAtuais[indiceModificado] = conteudoMudado;
-  // writeJson(livrosAtuais);
-  // return conteudoMudado;
+  let livrosAtuais = readJson();
+
+  const indiceModificado = livrosAtuais.findIndex(
+    (livro) => String(livro.id) === String(id),
+  );
+  if (indiceModificado === -1) return null;
+
+  const conteudoMudado = { ...livrosAtuais[indiceModificado], ...modificacoes };
+
+  livrosAtuais[indiceModificado] = conteudoMudado;
+
+  writeJson(livrosAtuais);
+  return conteudoMudado;
 }
 
 module.exports = {
